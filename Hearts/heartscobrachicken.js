36f7914aa02f0d54c5a27d1ecfe8ef72853e3a41
IntegralX1 <49178751+IntegralX1@users.noreply.github.com>
2023-10-31 20:26:26 -0600

    Updated JS with example from tutorial

--- Hearts/heartscobrachicken.js
+++ Hearts/heartscobrachicken.js
@@ -24,6 +24,9 @@ function (dojo, declare) {
     return declare("bgagame.heartscobrachicken", ebg.core.gamegui, {
         constructor: function(){
             console.log('heartscobrachicken constructor');
+
+            this.cardwidth = 72;
+            this.cardheight = 96;
               
             // Here, you can init the global variables of your user interface
             // Example:
@@ -56,6 +59,39 @@ function (dojo, declare) {
                 // TODO: Setting up players boards if needed
             }
             
+             // Player hand
+             this.playerHand = new ebg.stock();
+             this.playerHand.create(this, $('myhand'), this.cardwidth, this.cardheight);
+             this.playerHand.image_items_per_row = 13;
+            
+                // Create cards types:
+            for (var color = 1; color <= 4; color++) {
+                for (var value = 2; value <= 14; value++) {
+                    // Build card type id
+                    var card_type_id = this.getCardUniqueId(color, value);
+                    this.playerHand.addItemType(card_type_id, card_type_id, g_gamethemeurl + 'img/cards.jpg', card_type_id);
+                }
+            }
+
+                // Cards in player's hand
+                for ( var i in this.gamedatas.hand) {
+                    var card = this.gamedatas.hand[i];
+                    var color = card.type;
+                    var value = card.type_arg;
+                    this.playerHand.addToStockWithId(this.getCardUniqueId(color, value), card.id);
+                }
+    
+                // Cards played on table
+                for (i in this.gamedatas.cardsontable) {
+                    var card = this.gamedatas.cardsontable[i];
+                    var color = card.type;
+                    var value = card.type_arg;
+                    var player_id = card.location_arg;
+                    this.playCardOnTable(player_id, color, value, card.id);
+                }
+                
+                // Setup game notifications to handle (see "setupNotifications" method below)
+                this.setupNotifications();
             // TODO: Set up your game interface here, according to "gamedatas"
             
  
@@ -158,7 +194,37 @@ function (dojo, declare) {
         
         */
 
+            // Get card unique identifier based on its color and value
+        getCardUniqueId : function(color, value) {
+            return (color - 1) * 13 + (value - 2);
+        },
+        
 
+        playCardOnTable : function(player_id, color, value, card_id) {
+            // player_id => direction
+            dojo.place(this.format_block('jstpl_cardontable', {
+                x : this.cardwidth * (value - 2),
+                y : this.cardheight * (color - 1),
+                player_id : player_id
+            }), 'playertablecard_' + player_id);
+
+            if (player_id != this.player_id) {
+                // Some opponent played a card
+                // Move card from player panel
+                this.placeOnObject('cardontable_' + player_id, 'overall_player_board_' + player_id);
+            } else {
+                // You played a card. If it exists in your hand, move card from there and remove
+                // corresponding item
+
+                if ($('myhand_item_' + card_id)) {
+                    this.placeOnObject('cardontable_' + player_id, 'myhand_item_' + card_id);
+                    this.playerHand.removeFromStockById(card_id);
+                }
+            }
+
+            // In any case: move it to its final destination
+            this.slideToObject('cardontable_' + player_id, 'playertablecard_' + player_id).play();
+        },   
         ///////////////////////////////////////////////////
         //// Player's action
         
@@ -225,33 +291,56 @@ function (dojo, declare) {
             console.log( 'notifications subscriptions setup' );
             
             // TODO: here, associate your game notifications with local methods
+
+            dojo.subscribe('newHand', this, "notif_newHand");
+            dojo.subscribe('playCard', this, "notif_playCard");
             
-            // Example 1: standard notification handling
-            // dojo.subscribe( 'cardPlayed', this, "notif_cardPlayed" );
+            dojo.subscribe( 'trickWin', this, "notif_trickWin" );
+            this.notifqueue.setSynchronous( 'trickWin', 1000 );
+            dojo.subscribe( 'giveAllCardsToPlayer', this, "notif_giveAllCardsToPlayer" );
+            dojo.subscribe( 'newScores', this, "notif_newScores" );
             
-            // Example 2: standard notification handling + tell the user interface to wait
-            //            during 3 seconds after calling the method in order to let the players
-            //            see what is happening in the game.
-            // dojo.subscribe( 'cardPlayed', this, "notif_cardPlayed" );
-            // this.notifqueue.setSynchronous( 'cardPlayed', 3000 );
-            // 
         },  
         
         // TODO: from this point and below, you can write your game notifications handling methods
         
-        /*
-        Example:
+        notif_newHand : function(notif) {
+            // We received a new full hand of 13 cards.
+            this.playerHand.removeAll();
+
+            for ( var i in notif.args.cards) {
+                var card = notif.args.cards[i];
+                var color = card.type;
+                var value = card.type_arg;
+                this.playerHand.addToStockWithId(this.getCardUniqueId(color, value), card.id);
+            }
+        },
+
+        notif_playCard : function(notif) {
+            // Play a card on the table
+            this.playCardOnTable(notif.args.player_id, notif.args.color, notif.args.value, notif.args.card_id);
+        },
         
-        notif_cardPlayed: function( notif )
-        {
-            console.log( 'notif_cardPlayed' );
-            console.log( notif );
-            
-            // Note: notif.args contains the arguments specified during you "notifyAllPlayers" / "notifyPlayer" PHP call
-            
-            // TODO: play the card in the user interface.
-        },    
-        
-        */
+
+        notif_trickWin : function(notif) {
+            // We do nothing here (just wait in order players can view the 4 cards played before they're gone.
+        },
+        notif_giveAllCardsToPlayer : function(notif) {
+            // Move all cards on table to given table, then destroy them
+            var winner_id = notif.args.player_id;
+            for ( var player_id in this.gamedatas.players) {
+                var anim = this.slideToObject('cardontable_' + player_id, 'overall_player_board_' + winner_id);
+                dojo.connect(anim, 'onEnd', function(node) {
+                    dojo.destroy(node);
+                });
+                anim.play();
+            }
+        },
+        notif_newScores : function(notif) {
+            // Update players' scores
+            for ( var player_id in notif.args.newScores) {
+                this.scoreCtrl[player_id].toValue(notif.args.newScores[player_id]);
+            }
+        },
    });             
 });
